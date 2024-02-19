9f3f088e3d2b9926f0edc5f36325f0d55c76bf00
James <145905476+HerbalsGerbals@users.noreply.github.com>
2024-02-19 12:06:02 +1100

    Fixed Jumping - Emilio

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float horizontal;
+    public float speed = 5f;
+    public float jumpPower = 7f;
+    private Rigidbody2D rb;
 
-    private Rigidbody2D rbMovement;
-    
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private Transform groundChecker;
+   
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rbMovement = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if i want movement to stop immediately change "GetAxis" to "GetAxisRaw"
-        float dirX = Input.GetAxis("Horizontal");
-        rbMovement.velocity = new Vector2(dirX * 5f, rbMovement.velocity.y);
-
-        if (Input.GetButtonDown("Jump"))
+        horizontal = Input.GetAxisRaw("Horizontal");
         {
-            rbMovement.velocity = new Vector2(rbMovement.velocity.x, 7f);
+            if (Input.GetButtonDown("Jump") && IsGounded()) 
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
         }
         
-        
+
     }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed,rb.velocity.y); 
+    }
+    private bool IsGounded()
+    {
+        return Physics2D.OverlapCircle(groundChecker.position, 0.2f, ground);
+    }
+   
+
 }
