06fd61c91c04e53dd2d2270fcec076db98205165
James <145905476+HerbalsGerbals@users.noreply.github.com>
2024-02-26 12:31:18 +1100

    Added Level 3 (Emilio)

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,14 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+
+    public int currentLevelIndex = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            LoadNextLevel();
+            
         }
     }
 
+    public void LoadNextLevel()
+    {
+        currentLevelIndex++;
+        LoadLevelByIndex(currentLevelIndex);
+    }
+    private void LoadLevelByIndex(int index)
+    {
+        SceneManager.LoadScene(currentLevelIndex);
+    }
     // Update is called once per frame
     void Update()
     {
